29f3b8729276ea6c03a43c132a2faf5c20130ead
Ian Terrell <ian.terrell@gmail.com>
2010-02-10 01:42:55 -0500

    more controller speccing

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,21 +16,25 @@ describe GramsController do
   it "should use the show token as id to show" do
     get :show, :id => @gram.show_token
     assigns[:gram].should == @gram
+    response.should render_template("grams/show.html.erb")
   end
 
   it "should not use the id to confirm" do
     get :confirm, :id => @gram.id
     flash[:error].should == "Invalid confirmation code"
+    response.should redirect_to(root_path)
   end
 
   it "should use the confirm token as id to confirm" do
     get :confirm, :id => @gram.confirm_token
     flash[:success].should == "Confirmed!  Your Grumpy Gram has been sent to #{@gram.to_name}."
+    response.should redirect_to(root_path)
   end
 
   it "should not confirm a gram that has already been confirmed" do
     @gram.update_attribute :confirmed, true
     get :confirm, :id => @gram.confirm_token
     flash[:error].should == "This GrumpyGram has already been sent!"
+    response.should redirect_to(root_path)
   end
 end
