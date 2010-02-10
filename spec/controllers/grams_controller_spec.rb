580e13aff971a3bab4e41a3ce488be7a21b0c766
Ian Terrell <ian.terrell@gmail.com>
2010-02-10 01:52:38 -0500

    more spec work as an example

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,13 +4,30 @@ describe GramsController do
   before(:all) do
     @gram = Factory.create(:gram)
   end
+  
+  it { should route(:get, "/grams/new").to(:action => :new) }
+  it { should route(:post, "/grams").to(:action => :create) }
+  it { should route(:get, "/grams/1").to(:action => :show, :id => 1) }
+  it { should route(:get, "/grams/token").to(:action => :show, :id => "token") }
+
+  it "should not route index" do
+    lambda { get :index }.should raise_error(ActionController::RoutingError)
+  end
+  
+  it "should not route edit" do
+    lambda { get :edit, :id => 1 }.should raise_error(ActionController::RoutingError)
+  end
+  
+  it "should not route update" do
+    lambda { put :update, :id => 1 }.should raise_error(ActionController::RoutingError)
+  end
+  
+  it "should not route destroy" do
+    lambda { delete :destroy, :id => 1 }.should raise_error(ActionController::RoutingError)
+  end
 
   it "should not use the id to show" do
-    begin
-      get :show, :id => @gram.id
-      fail
-    rescue ActiveRecord::RecordNotFound
-    end
+    lambda { get :show, :id => @gram.id }.should raise_error(ActiveRecord::RecordNotFound)
   end
   
   it "should use the show token as id to show" do
@@ -22,19 +39,55 @@ describe GramsController do
   it "should not use the id to confirm" do
     get :confirm, :id => @gram.id
     flash[:error].should == "Invalid confirmation code"
+  end
+  
+  it "should redirect to root on bad confirm" do
+    get :confirm, :id => "asdf"
     response.should redirect_to(root_path)
   end
 
   it "should use the confirm token as id to confirm" do
     get :confirm, :id => @gram.confirm_token
     flash[:success].should == "Confirmed!  Your Grumpy Gram has been sent to #{@gram.to_name}."
-    response.should redirect_to(root_path)
+  end
+  
+  it "should send the mail to the recipient on confirmation" do
+    GramMailer.should_receive(:deliver_notification)
+    get :confirm, :id => @gram.confirm_token
+  end
+  
+  describe "on successful confirmation" do
+    before(:each) do
+      get :confirm, :id => @gram.confirm_token
+    end
+    
+    it "should redirect to root" do
+      response.should redirect_to(root_path)
+    end
+    
+    it "should set confirmed flag" do
+      @gram.reload.should be_confirmed
+    end
   end
 
-  it "should not confirm a gram that has already been confirmed" do
+  describe "if already confirmed" do
+    before(:each) do
+      @gram.update_attribute :confirmed, true
+      get :confirm, :id => @gram.confirm_token      
+    end
+    
+    it "should let the user know it's already been sent" do
+      flash[:error].should == "This GrumpyGram has already been sent!"
+    end
+    
+    it "should redirect to root" do
+      response.should redirect_to(root_path)
+    end
+  end
+  
+  it "should not resend mail if already confirmed" do
     @gram.update_attribute :confirmed, true
+    GramMailer.should_not_receive(:deliver_notification)
     get :confirm, :id => @gram.confirm_token
-    flash[:error].should == "This GrumpyGram has already been sent!"
-    response.should redirect_to(root_path)
   end
 end
