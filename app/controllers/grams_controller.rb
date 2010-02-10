49c4ac43533ec9a57e5125353edd4f94279e2000
Ian Terrell <ian.terrell@gmail.com>
2010-02-09 19:50:57 -0500

    redesigned send the gram form

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,10 @@
 class GramsController < ApplicationController
   resource_controller
   
+  before_filter :get_gram_templates, :only => [:new, :create]
+
+private
+  def get_gram_templates
+    @gram_templates = GramTemplate.find :all
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('.gram-template-preview').click(function(){
+    $('#gram_gram_template_id').val($(this).attr('gram_template_id'));
+  });
+});
