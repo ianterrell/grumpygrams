e5eed7041550a77997d8c019f8096547b993a604
Ian Terrell <ian.terrell@gmail.com>
2010-02-14 14:44:29 -0500

    only including icons we use

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,3 @@
 class MainController < ApplicationController
   def index; end
-  def icons
-    flash.now[:success] = "This is success."
-    flash.now[:notice] = "This is notice."
-    flash.now[:error] = "This is error."
-  end # TODO: remove me
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => "main"
   
-  # TODO: Remove -- temporary --
-  map.connect "/_icons", :controller => "main", :action => "icons"
-  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
