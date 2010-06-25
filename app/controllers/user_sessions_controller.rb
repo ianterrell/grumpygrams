21c32578f5617e13dcef1d2457efe51a2d5f5589
Jeff Bozek <jeff.bozek@gmail.com>
2010-06-25 16:41:47 -0500

    First run at oauth2 with minifb

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,8 +3,10 @@ require 'mini_fb'
 class UserSessionsController < ApplicationController
   
   def facebook_connect
-    @fb_info = MiniFB.parse_cookie_information('fd577fc6f9d8d122717f0fdd6112e234', cookies)
-    redirect_to :grams
+    access_token_hash = MiniFB.oauth_access_token(137471566265661, facebook_connect_url, "9db8f39e7d7085c5fb7a42f94ae74451", params[:code])
+    @access_token = access_token_hash["access_token"]
+    logger.info "#{@access_token}"
+    redirect_to :root
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
+require 'mini_fb'
+
 module ApplicationHelper
   
   def logged_in?
     current_user
   end
   
+  def oauth_url
+    @oauth_url = MiniFB.oauth_url(137471566265661, # your Facebook App ID
+                                facebook_connect_url, # redirect url
+                                :scope=>MiniFB.scopes.join(","))
+  end
 end
