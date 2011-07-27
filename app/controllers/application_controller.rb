46ec226f3f04ec5612848fce00b5a4017bfb584e
Jeff Bozek <jeff.bozek@gmail.com>
2011-07-26 19:08:56 -0500

    redirecting root to www.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_uri
+
   helper :all
   protect_from_forgery
   layout 'application'
+  
+  def check_uri
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) && Rails.env.production?
+  end
 end
